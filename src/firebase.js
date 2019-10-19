67bbdb500bc279f9845c7cebecfb9454ac8fc616
Ilya Petrov <dnamix1@gmail.com>
2019-10-19 14:44:06 +0300

    have created folder structure, basic layout components, fired up firebase connection and built tasks filtering custom hooks

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = firebase.initializeApp({
+  apiKey: "AIzaSyA_oLfRBDIOsaXfjebCC0axN4CPkN-M1DY",
+  authDomain: "perfectplanner-82d07.firebaseapp.com",
+  databaseURL: "https://perfectplanner-82d07.firebaseio.com",
+  projectId: "perfectplanner-82d07",
+  storageBucket: "perfectplanner-82d07.appspot.com",
+  messagingSenderId: "44845674118",
+  appId: "1:44845674118:web:caca606c56b99699ed2b07",
+  measurementId: "G-CWNN2GHK7H"
+});
+
+firebase.analytics();
+
+export { firebaseConfig as firebase };
