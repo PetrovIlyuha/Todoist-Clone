50d9abc95d66a713f45ad2d29fc86f1559274fea
Ilya Petrov <dnamix1@gmail.com>
2019-10-21 13:29:56 +0300

    created IndividualProject Component, managed to implement deletion from sidebar with modal & wrote context API part

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Checkbox } from './Checkbox';
 import { useTasks } from '../hooks/index';
+import { collatedTasks } from '../constants/index';
+import { getTitle, getCollatedTitle, collatedTasksExist } from '../helpers/index';
+import { useSelectedProjectValue, useProjectsValue } from '../context/index';
 
 export const Tasks = () => {
   const { tasks } = useTasks("1");
-
-  console.log(tasks)
 
   let projectName = '';
 
